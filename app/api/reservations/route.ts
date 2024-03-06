a70f3e8fc9c5a1d11116fadb3345b822493584ad
Gaduh Hartawan <gaduhhartaone@gmail.com>
2024-03-06 10:17:05 +0700

    feat: fix possibly null for currentUser

--- app/api/reservations/route.ts
+++ app/api/reservations/route.ts
@@ -6,6 +6,10 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
 
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
   const body = await request.json();
 
   const { listingId, startDate, endDate, totalPrice } = body;
